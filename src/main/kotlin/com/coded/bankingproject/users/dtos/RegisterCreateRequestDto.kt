01a8d7d592c6a330a7415dc86ab2ef4202fcf2fd
Yousef <yayalkandari@gmail.com>
2025-04-15 18:53:32 +0300

    added createUser validation

--- src/main/kotlin/com/coded/bankingproject/users/dtos/RegisterCreateRequestDto.kt
+++ src/main/kotlin/com/coded/bankingproject/users/dtos/RegisterCreateRequestDto.kt
@@ -2,11 +2,15 @@ package com.coded.bankingproject.users.dtos
 
 import com.coded.bankingproject.domain.entities.UserEntity
 import jakarta.validation.constraints.*
+import org.hibernate.validator.constraints.Length
 
 data class RegisterCreateRequestDto(
     @field:NotBlank(message = "Username is required")
+    @field:Length(min = 3, message = "Username is too short")
     val username: String,
     @field:NotBlank(message = "Password is required")
+    @field:Length(min = 6, message = "Password is too short")
+    @field:Pattern(regexp = """(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*""", message = "Password is too simple")
     val password: String,
 )
 
